e83c8f4f51cdd8c333d0debb31d74c6ef2c16da9
taladjidi <taladjidi@gmail.com>
2021-07-28 17:51:18 +0200

    removed useless commented block

--- bresenham.py
+++ bresenham.py
@@ -5,26 +5,6 @@ import time
 # function for line generation
 
 
-# @jit(nopython=True)
-# def bresenham(x1, y1, x2, y2):
-#
-#     m_new = 2 * (y2 - y1)
-#     slope_error_new = m_new - (x2 - x1)
-#
-#     y = y1
-#     coords = []
-#     for x in range(x1, x2+1):
-#         coords.append([x, y])
-#         # Add slope to increment angle formed
-#         slope_error_new += m_new
-#
-#         # Slope error reached limit, time to
-#         # increment y and update slope error.
-#         if (slope_error_new >= 0):
-#             y += 1
-#             slope_error_new -= 2 * (x2 - x1)
-#
-#     return coords
 @jit(nopython=True)
 def bresenham(x1, y1, x2, y2):
     """Bresenham's Line Algorithm
