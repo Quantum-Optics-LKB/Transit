5b693521c5fd1ab01f83ecd11056d27f1388f18d
taladjidi <taladjidi@gmail.com>
2021-09-13 18:32:18 +0200

    No more weird symetry : inverted x y ...

--- do_v.jl
+++ do_v.jl
@@ -6,22 +6,24 @@ using PyPlot
 t_tot0 = time()
 # global variables to be set from python side through Julia "Main" namespace
 const N_real = 5000
-const N_grid = 256
+const N_grid = 128
 const N_t = 1000
 const N_v = 20
 const T = 150+273
 const m87 = 1.44316060e-25
 const k_B = 1.38064852e-23
-const Gamma = 38107518.88804419 + 0*im
-const Omega13 = 135886166.68478012 + 0*im
-const Omega23 = 0.021626955125691984e9 + 0*im
+const Gamma = 38107518.88804419 + 0.0*im
+const Omega13 = 135886166.68478012 + 0.0*im
+const Omega23 = 0.021626955125691984e9 + 0.0*im
 const gamma21tilde = 321033.05667335045+42939288389.26529*im
 const gamma31tilde = 46564466.011063695+61788844310.80405*im
 const gamma32tilde = 46564466.011063695+18849555921.538757*im
-const waist = 1.0e-3 + 0*im
+const waist = 1.0e-3 + 0.0*im
 const r0 = 1.5*waist
-const window = 3*waist
-const x0 = ComplexF64[5/8 + 0*im, 3/8+ 0*im, 0+ 0*im, 0+ 0*im, 0+ 0*im, 0+ 0*im, 0+ 0*im, 0+ 0*im]
+const window = 3.0*waist
+const x0 = ComplexF64[5/8 + 0.0*im, 3/8 + 0.0*im, 0.0 + 0.0*im,
+                      0.0 + 0.0*im, 0.0 + 0.0*im, 0.0 + 0.0*im,
+                      0.0 + 0.0*im, 0.0+ 0.0*im]
 const k = 2*pi/780.241e-9
 const v0 = 40.0
 const v1 = 800.0
@@ -119,21 +121,28 @@ function draw_vz(v::Float64)::Float64
     return vz
 end
 
+@inbounds begin
+@fastmath begin
 function f!(dy::Array{ComplexF64, 1}, x::Array{ComplexF64, 1},
             p::Array{ComplexF64, 1}, t::Float64)
     r_sq = (p[4]+p[2]*p[1]*t - p[13])*(p[4]+p[2]*p[1]*t - p[13]) + (p[5]+p[3]*p[1]*t - p[13])*(p[5]+p[3]*p[1]*t - p[13])
     Om23 = p[8] * exp(-r_sq/(2.0*p[12]*p[12]))
     Om13 = p[7] * exp(-r_sq/(2.0*p[12]*p[12]))
-    dy[1] = (-p[6]/2)*x[1]-(p[6]/2)*x[2]+(im*conj(Om13)/2)*x[5]-(im*Om13/2)*x[6]+p[6]/2
-    dy[2] = (-p[6]/2)*x[1]-(p[6]/2)*x[2]+(im*conj(Om23)/2)*x[7]-(im*Om23/2)*x[8]+p[6]/2
-    dy[3] = -p[9]*x[3]+(im*conj(Om23)/2)*x[5]-(im*Om13/2)*x[8]
-    dy[4] = -conj(p[9])*x[4] - (im*Om23/2)*x[6] + (im*conj(Om13)/2)*x[7]
-    dy[5] = im*Om13*x[1] + (im*Om13/2)*x[2] + (im*Om23/2)*x[3] - p[10]*x[5]-im*Om13/2
-    dy[6] = -im*conj(Om13)*x[1]-im*(conj(Om13)/2)*x[2]-(im*conj(Om23)/2)*x[4]-conj(p[10])*x[6]+im*conj(Om13)/2
-    dy[7] = (im*Om23/2)*x[1]+im*Om23*x[2]+(im*Om13/2)*x[4]-p[11]*x[7] - im*Om23/2
-    dy[8] = (-im*conj(Om23)/2)*x[1]-im*conj(Om23)*x[2]-(im*conj(Om13)/2)*x[3]-conj(p[11])*x[8]+im*conj(Om23)/2
+    dy[1] = (-p[6]/2.0)*x[1]-(p[6]/2.0)*x[2]+(im*conj(Om13)/2.0)*x[5]-(im*Om13/2)*x[6]+p[6]/2.0
+    dy[2] = (-p[6]/2.0)*x[1]-(p[6]/2.0)*x[2]+(im*conj(Om23)/2.0)*x[7]-(im*Om23/2)*x[8]+p[6]/2.0
+    dy[3] = -p[9]*x[3]+(im*conj(Om23)/2.0)*x[5]-(im*Om13/2.0)*x[8]
+    dy[4] = -conj(p[9])*x[4] - (im*Om23/2.0)*x[6] + (im*conj(Om13)/2.0)*x[7]
+    dy[5] = im*Om13*x[1] + (im*Om13/2.0)*x[2] + (im*Om23/2.0)*x[3] - p[10]*x[5]-im*Om13/2.0
+    dy[6] = -im*conj(Om13)*x[1]-im*(conj(Om13)/2.0)*x[2]-(im*conj(Om23)/2.0)*x[4]-conj(p[10])*x[6]+im*conj(Om13)/2.0
+    dy[7] = (im*Om23/2.0)*x[1]+im*Om23*x[2]+(im*Om13/2.0)*x[4]-p[11]*x[7] - im*Om23/2.0
+    dy[8] = (-im*conj(Om23)/2.0)*x[1]-im*conj(Om23)*x[2]-(im*conj(Om13)/2.0)*x[3]-conj(p[11])*x[8]+im*conj(Om23)/2.0
+end
+end
 end
 
+
+@inbounds begin
+@fastmath begin
 function f_jac!(J::Array{ComplexF64, 2}, x::Array{ComplexF64, 1},
             p::Array{ComplexF64, 1}, t::Float64)
     r_sq = (p[4]+p[2]*p[1]*t - p[13])^2 + (p[5]+p[3]*p[1]*t - p[13])^2
@@ -204,18 +213,8 @@ function f_jac!(J::Array{ComplexF64, 2}, x::Array{ComplexF64, 1},
     J[8, 7] = 0.0 * im
     J[8, 8] = -conj(p[11])
 end
-
-# p0 = ComplexF64[1.0 + 0*im, 1.0 + 0*im, 1.0 + 0*im, 1.0 + 0*im, 1.0 + 0*im,
-#      Gamma, Omega13,
-#      Omega23, gamma21tilde,
-#      gamma31tilde - im*k*0.0,
-#      gamma32tilde - im*k*0.0, waist, r0]
-# J = zeros(ComplexF64, (8, 8))
-# dy = similar(x0)
-# @code_warntype f!(dy, x0, p0, 1.0)
-# @code_warntype f_jac!(J, x0, p0, 1.0)
-# @time f!(dy, x0, p0, 1.0)
-# @time f_jac!(J, x0, p0, 1.0)
+end
+end
 
 
 #TODO directly put an accumulator array of size (N_grid, N_grid)
@@ -228,18 +227,25 @@ counter_grid_total = zeros(Int32, (N_grid, N_grid))
 grid_weighted = zeros(ComplexF64, (N_grid, N_grid))
 normalized = zeros(Float64, (N_grid, N_grid))
 Vs = collect(LinRange{Float64}(v0, v1, N_v))
-pv = sqrt(2/pi)*((m87/(k_B*T))^(3/2)).*Vs.^2 .*exp.(-m87*Vs.^2/(2*k_B*T))
+pv = sqrt(2.0/pi)*((m87/(k_B*T))^(3.0/2.0)).*Vs.^2.0 .*exp.(-m87*Vs.^2.0/(2.0*k_B*T))
 Xs = zeros(ComplexF64, (N_t, N_real))
 Ts = zeros(Float64, (N_t, N_real))
 v_perps = zeros(Float64, N_real)
 paths = [[] for i=1:N_real]
-
+# for convenience
+global coords = Array{Tuple{Int32, Int32, Int32, Int32}}(undef, N_real)
+Threads.@threads for i = 1:N_real
+    iinit, jinit, ifinal, jfinal = choose_points()
+    coords[i] = (iinit, jinit, ifinal, jfinal)
+    paths[i] = bresenham(jinit, iinit, jfinal, ifinal)
+end
 for (index_v, v) in enumerate(Vs)
     function prob_func(prob, i, repeat)
         # change seed of random number generation as the solver uses multithreading
-        iinit, jinit, ifinal, jfinal = choose_points()
+        iinit, jinit, ifinal, jfinal = coords[i]
+        # iinit, jinit, ifinal, jfinal = choose_points()
         vz = draw_vz(v)
-        v_perp = sqrt(v^2 - vz^2)
+        v_perp = sqrt(v^2.0 - vz^2.0)
         xinit = jinit*window/N_grid
         yinit = iinit*window/N_grid
         xfinal = jfinal*window/N_grid
@@ -256,7 +262,8 @@ for (index_v, v) in enumerate(Vs)
             u0 = u1 = 0
             new_tfinal = hypot((xfinal-xinit), (yfinal-yinit))/abs(vz)
         end
-        new_p = ComplexF64[v_perp + 0*im, u0 + 0*im, u1 + 0*im, xinit + 0*im, yinit + 0*im,
+        new_p = ComplexF64[v_perp + 0.0*im, u0 + 0.0*im, u1 + 0.0*im,
+                 xinit + 0.0*im, yinit + 0.0*im,
                  Gamma, Omega13, Omega23, gamma21tilde, gamma31tilde - im*k*vz,
                  gamma32tilde - im*k*vz, waist, r0]
         new_tspan = (0.0, Float64(new_tfinal))
@@ -264,19 +271,19 @@ for (index_v, v) in enumerate(Vs)
         remake(prob, p=new_p, tspan=new_tspan, saveat=tsave)
     end
     # instantiate a problem
-    p = ComplexF64[1.0 + 0*im, 1.0 + 0*im, 1.0 + 0*im, 1.0 + 0*im, 1.0 + 0*im,
+    p = ComplexF64[1.0 + 0.0*im, 1.0 + 0.0*im, 1.0 + 0.0*im, 1.0 + 0.0*im, 1.0 + 0.0*im,
          Gamma, Omega13,
          Omega23, gamma21tilde,
          gamma31tilde - im*k*0.0,
          gamma32tilde - im*k*0.0, waist, r0]
     tspan = (0.0, 1.0)
-    tsave = collect(LinRange{Float64}(0.0, 1.0, N_t))
+    tsave = collect(LinRange{Float64}(tspan[1], tspan[2], N_t))
     prob = ODEProblem{true}(f!, x0, tspan, p, jac=f_jac!, saveat=tsave)
     ensembleprob = EnsembleProblem(prob, prob_func=prob_func)
     # run once on small system to try and speed up compile time
     if index_v==1
         t0 = time()
-        sol = solve(ensembleprob, Rodas5(autodiff=false), EnsembleThreads(),
+        sol = solve(ensembleprob, Rodas4(autodiff=false), EnsembleThreads(),
                           trajectories=2, save_idxs=7,
                           abstol=1e-6, reltol=1e-3)
         t1 = time()
@@ -284,7 +291,7 @@ for (index_v, v) in enumerate(Vs)
 
     end
     t0 = time()
-    sol = solve(ensembleprob, Rodas5(autodiff=false), EnsembleThreads(),
+    sol = solve(ensembleprob, Rodas4(autodiff=false), EnsembleThreads(),
                       trajectories=N_real, save_idxs=7,
                       abstol=1e-6, reltol=1e-3)
     t1 = time()
@@ -292,31 +299,11 @@ for (index_v, v) in enumerate(Vs)
     # @time sol = solve(ensembleprob, BS3(), EnsembleGPUArray(), trajectories=N_real)
     t0 = time()
     Threads.@threads for i in 1:N_real
-        # what I would like
         Xs[:, i] .= sol[i].u
         Ts[:, i] .= sol[i].t
         v_perps[i] = sol[i].prob.p[1]
-        local u0 = sol[i].prob.p[2]
-        local u1 = sol[i].prob.p[3]
-        local xinit = sol[i].prob.p[4]
-        local yinit = sol[i].prob.p[5]
-        local tfinal = sol[i].prob.tspan[2]
-        local xfinal = xinit+u0*v_perps[i]*tfinal
-        local yfinal = yinit+u1*v_perps[i]*tfinal
-        local iinit = Int32(round(real(yinit)*(N_grid/window), digits=1))
-        local jinit = Int32(round(real(xinit)*(N_grid/window), digits=1))
-        local ifinal = Int32(round(real(yfinal)*(N_grid/window), digits=1))
-        local jfinal = Int32(round(real(xfinal)*(N_grid/window), digits=1))
-        local path = bresenham(jinit, iinit, jfinal, ifinal)
-        paths[i] = path
     end
-    # Reformat xs as a proper multidimensional Array for easier indexing
-    # Threads.@threads for i = 1:N_real
-    #     for j = 1:N_t
-    #         Xs[j, i] = xs[i][j]
-    #         Ts[j, i] = ts[i][j]
-    #     end
-    # end
+
     t1 = time()
     println("Time spent reformatting the output : $(t1-t0) s")
     function treat_coord!(i::Int64, coord::Array{Int32, 1}, iinit::Int32,
@@ -343,8 +330,8 @@ for (index_v, v) in enumerate(Vs)
     global grid .= zeros(ComplexF64, (N_grid, N_grid))
     global counter_grid .= zeros(Int32, (N_grid, N_grid))
     Threads.@threads for i = 1:N_real
-        local iinit = paths[i][1][1]
-        local jinit = paths[i][1][2]
+        local iinit = paths[i][1][2]
+        local jinit = paths[i][1][1]
         m_func(coord) = treat_coord!(i, coord, iinit, jinit, grid, counter_grid)
         map(m_func, paths[i])
     end
@@ -353,9 +340,7 @@ for (index_v, v) in enumerate(Vs)
     grid_weighted .+= (grid./counter_grid) * pv[index_v]
     counter_grid_total .+= counter_grid
 end
-# weighted_grid = real.(grids).*pv
-# pol = sum(weighted_grid, dims=1)
-# total_counts = sum(counter_grids, dims=1)
+
 pol = real.(grid_weighted)
 println("Total time elapsed : $(time()-t_tot0) s")
 fig, ax = subplots(1, 2)
